1789f83e22538a95f7321f8b1b7a868da6a1f6e6
baynobu <darmawanhanif30@gmail.com>
2026-03-05 00:14:40 +0700

    upd 6 eloquent

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,18 +11,18 @@ class UserController extends Controller
     public function index()
     {
         // tambah data user dengan Eloquent Model
-        // $data = [
-        //     'username' => 'customer-1',
-        //     'nama' => 'Pelanggan',
-        //     'password' => Hash::make('12345'),
-        //     'level_id' => 4
-        // ];
-        // UserModel::insert($data); // tambahkan data ke tabel m_user
-
         $data = [
-            'nama' => 'Pelanggan Pertama',
+            'username' => 'customer-1',
+            'nama' => 'Pelanggan',
+            'password' => Hash::make('12345'),
+            'level_id' => 3
         ];
-        UserModel::where('username', 'customer-1')->update($data); // update data user
+        UserModel::insert($data); // tambahkan data ke tabel m_user
+
+        // $data = [
+        //     'nama' => 'Pelanggan Pertama',
+        // ];
+        // UserModel::where('username', 'customer-1')->update($data); // update data user
 
         // coba akses model UserModel
         $user = UserModel::all(); // ambil semua data dari tabel m_user
