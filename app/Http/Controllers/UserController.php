b0612c52c364c8a9331764adb2e24d5952af79b7
baynobu <darmawanhanif30@gmail.com>
2026-03-11 19:21:03 +0700

    2.2 Not Found Exceptions

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,22 +10,32 @@ class UserController extends Controller
 {
     public function index()
     {
-        // tambah data user dengan Eloquent Model
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
-        UserModel::create($data); // tambahkan data ke tabel m_user
+        // $user = UserModel::findOr(1, ['username', 'nama'], function(){
+        //     abort(404);
+        // });
 
-        // $data = [
-        //     'nama' => 'Pelanggan Pertama',
-        // ];
-        // UserModel::where('username', 'customer-1')->update($data); // update data user
-
-        // coba akses model UserModel
-        $user = UserModel::all(); // ambil semua data dari tabel m_user
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
         return view('user', ['data' => $user]);
     }
+
+    // public function index()
+    // {
+    //     // tambah data user dengan Eloquent Model
+    //     $data = [
+    //         'level_id' => 2,
+    //         'username' => 'manager_tiga',
+    //         'nama' => 'Manager 3',
+    //         'password' => Hash::make('12345')
+    //     ];
+    //     UserModel::create($data); // tambahkan data ke tabel m_user
+
+    //     // $data = [
+    //     //     'nama' => 'Pelanggan Pertama',
+    //     // ];
+    //     // UserModel::where('username', 'customer-1')->update($data); // update data user
+
+    //     // coba akses model UserModel
+    //     $user = UserModel::all(); // ambil semua data dari tabel m_user
+    //     return view('user', ['data' => $user]);
+    // }
 }
